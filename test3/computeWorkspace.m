e368c501c5d37dfeb24024b0c364e0b5e4135c27
varunsan <varunsanghvi2000@gmail.com>
2017-09-18 16:59:00 -0400

    final changes

--- test3/computeWorkspace.m
+++ test3/computeWorkspace.m
@@ -1,9 +1,7 @@
 % Place your code for computing the workspace of the Lynx robot here.
 
-%qvar =[0,0,0,0,0,10];
-for i = -1.2:0.1:1.2
-    lynxServo(0,0,0,0,i,10);
+
+for i=-1.4:0.1:1.4
+    lynxServo(i,i,0,0,0,10);
     pause(1);
 end
-
-
